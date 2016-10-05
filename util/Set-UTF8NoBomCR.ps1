9be62a58fa51cbc9fc1bdd55e2493610a46754e8
wub <wub@landicorp.com>
2016-10-06 00:53:39 +0800

    debugging.

--- util/Set-UTF8NoBomCR.ps1
+++ util/Set-UTF8NoBomCR.ps1
@@ -1,13 +1,13 @@
-.\Out-FileUtf8NoBom.ps1
-
 dir ..\source\_posts\*.md | ForEach-Object {
-    del temp.txt -ErrorAction SilentlyContinue
+    # del D:\temp.txt -ErrorAction SilentlyContinue
     $file = $_
     $encoding = .\Get-FileEncoding.ps1 $file.FullName
+    if ($encoding.BodyName.StartsWith('utf-')) { $encoding = $encoding.BodyName.Replace('utf-', 'UTF') }
+    if ($encoding.BodyName -eq 'ASCII') { $encoding = 'Ascii' }
     echo $encoding
     gc $file -Encoding $encoding | ForEach-Object {
-        Out-FileUtf8NoBom temp.txt $True $False $null $_
+        .\Out-FileUtf8NoBom.ps1 D:\temp.txt $False $False $null $_
     }
 
-    .\Set-Eol.ps1 'unix' temp.txt
+    .\Set-Eol.ps1 'unix' 'D:\temp.txt'
 }
